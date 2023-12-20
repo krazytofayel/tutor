eeffd61299527f5e6ab6037b3d9fbd7a0dded4f4
krazytofayel <tofayel@krazyit.com>
2023-12-20 16:08:48 +0600

    (feat):eslint

--- src/app/AllForm/RegisterTutor/ProfileDetailsFrom/page.jsx
+++ src/app/AllForm/RegisterTutor/ProfileDetailsFrom/page.jsx
@@ -1,10 +1,11 @@
+'use client'
 
-
-import { Box, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Typography } from '@mui/material'
+import ImageUpload from '@/app/Components/ShareAbleComponent/Helper/ImageUpload/ImageUpload';
+import { Box, FormControl, FormControlLabel, FormHelperText, FormLabel, Input, Radio, RadioGroup, TextareaAutosize, Typography } from '@mui/material'
 import { Button } from 'flowbite-react'
 
 
-const ProfileDetailsForm = ({ handleNext, handleBack, activeStep }) => {
+const ProfileDetailsForm = ({ handleNext, handleBack, activeStep, isStepOptional, handleSkip, steps }) => {
 
   // const handleSubmit = (e) => {
   //   e.preventDefault(); 
@@ -21,158 +22,46 @@ const ProfileDetailsForm = ({ handleNext, handleBack, activeStep }) => {
     <div className='w-full'>
 
 
-      <form className="max-w-3xl mx-auto p-5  bg-[#f9f9fa] rounded-lg border-t-4 border-t-[#089bab] border-b-4 border-b-[#089bab]  "> 
-      <Typography sx={{ mt: 2, mb: 1 }}><span className='text-sm font-bold text-gray-500'>Step</span>  <span className='text-sm font-bold text-gray-500'> {activeStep + 1}</span>  <span className='text-sm font-bold text-gray-500'>oF</span>  <span className='text-sm font-bold text-gray-500'> {activeStep + 1}</span> </Typography> 
+
+      <form className="max-w-3xl mx-auto p-5  bg-[#f9f9fa] rounded-lg border-t-4 border-t-[#089bab] border-b-4 border-b-[#089bab]  ">
+        <Typography sx={{ mt: 2, mb: 1 }}><span className='text-sm font-bold text-gray-500'>Step</span>  <span className='text-sm font-bold text-gray-500'> {activeStep + 1}</span>  <span className='text-sm font-bold text-gray-500'>oF</span>  <span className='text-sm font-bold text-gray-500'> {activeStep + 1}</span> </Typography>
         <div>
 
-          <p className='font-medium text-lg'>Profile Details</p>
-          <p className='text-sm mb-5 mt-2'>If you meet tutor2u <a href="http://">minimum requirements, complete the registration form to register and proceed to create your profile on tutor. Fields marked with * are required to be completed.</a> </p>
+          <p className='font-medium text-lg'>Public Profile</p>
+          <p className='text-sm mb-5 mt-2'>Now create your public profile which will be shown to prospective students on tutor2u.</p>
         </div>
-        <div className='grid grid-cols-12 gap-5'>
-          <div className='col-span-6'><label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required /></div>
-          <div className='col-span-6'><label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required /></div>
+
+        <div class="mb-5">
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">1. Upload Profile Picture*</label>
+
+          <ImageUpload/>
+         
+          <FormHelperText>
+
+          svg,img,pdf
+          </FormHelperText>
         </div>
 
 
 
         <div class="mb-5">
-          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-          <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">2. Create your Tagline*</label>
+          <Input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+          <FormHelperText>
+
+            Maximum 100 characters
+          </FormHelperText>
         </div>
 
         <div class="mb-5">
-          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-          <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">3. Tell the world about you*</label>
+          <TextareaAutosize placeholder='This is where you introduce who you are, flaunt your experience, academic history, qualifications and interests to captivate students. Tell them why you should be their tutor.' className='w-full border rounded' minRows={4} sx={{
+            borderColor: '#86b7fe',
+
+          }}
+          />
         </div>
-        <div class="mb-5">
-          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">How did you hear about tutor? *</label>
-          <div>
-            <div className='grid grid-cols-12 gap-5'>
 
-              <div className='col-span-6'>
-                <FormControl component="fieldset">
-                  {/* <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel> */}
-                  <RadioGroup
-                    aria-labelledby="demo-radio-buttons-group-label-1"
-                    defaultValue="Google Search"
-                    name="radio-buttons-group-1"
-                    onChange={handleRadioChange}
-                  >
-                    <FormControlLabel value="Google Search" control={<Radio size="small" />} label={
-                      <span className="text-sm"> Google</span>
-                    } />
-                    <FormControlLabel value="Gumtree" control={<Radio size="small" />} label="Gumtree" />
-                    <FormControlLabel value="Indeed" control={<Radio size="small" />} label="Indeed" />
-                    <FormControlLabel value="Jora" control={<Radio size="small" />} label="Jora" />
-                    <FormControlLabel value="Recommended by a friend" control={<Radio size="small" />} label="Recommended by a friend" />
-                    <FormControlLabel value="Other" control={<Radio size="small" />} label="Other" />
-                  </RadioGroup>
-                </FormControl>
-              </div>
-              <div className='col-span-6'>
-                <FormControl component="fieldset">
-
-                  <RadioGroup
-                    aria-labelledby="demo-radio-buttons-group-label-1"
-                    defaultValue="Google Search"
-                    name="radio-buttons-group-right"
-                    onChange={handleRadioChange}
-                  >
-                    <FormControlLabel value="LinkedIn" control={<Radio size="small" />} label="LinkedIn" />
-                    <FormControlLabel value="Facebook" control={<Radio size="small" />} label="Facebook" />
-                    <FormControlLabel value="Facebook Ad" control={<Radio size="small" />} label="Facebook Ad" />
-                    <FormControlLabel value="Text message" control={<Radio size="small" />} label="Text message" />
-                    <FormControlLabel value="Email from tutor" control={<Radio size="small" />} label="Email from tutor" />
-                    <FormControlLabel value="University website" control={<Radio size="small" />} label="University website" />
-                  </RadioGroup>
-                </FormControl>
-              </div>
-
-
-            </div>
-
-            <hr />
-            <div>
-
-              <div className="flex items-center mt-5 mb-5">
-                <div className="flex items-center justify-center h-5">
-                  <input
-                    id="remember"
-                    type="checkbox"
-                    value=""
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                    required
-                  />
-                </div>
-                <label
-                  htmlFor="remember"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  I acknowledge and agree that my registration as a tutor is subject to review and approval by the tutor support team, and I may be asked for further information or to verify details as part of the application process.
-                </label>
-              </div>
-
-              <div className="flex items-center mb-5">
-                <div className="flex items-center justify-center h-5">
-                  <input
-                    id="remember"
-                    type="checkbox"
-                    value=""
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                    required
-                  />
-                </div>
-                <label
-                  htmlFor="remember"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  I confirm that I am 18 or older and live in and have a right to work in Australia
-                </label>
-              </div>
-              <div className="flex items-center mb-5">
-                <div className="flex items-center justify-center h-5">
-                  <input
-                    id="remember"
-                    type="checkbox"
-                    value=""
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                    required
-                  />
-                </div>
-                <label
-                  htmlFor="remember"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  I agree to tutor <a href="">Terms & Conditions</a>  and that I will schedule and seek payment for all lessons through tutor and will not accept, request, infer or solicit payment outside of tutor platform
-                </label>
-              </div>
-              <div className="flex items-center mb-5">
-                <div className="flex items-center justify-center h-5">
-                  <input
-                    id="remember"
-                    type="checkbox"
-                    value=""
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                    required
-                  />
-                </div>
-                <label
-                  htmlFor="remember"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  I have not been convicted or accused of nor have I been involved in the commission of any criminal offences, including but not limited to events involving children that would make me unsuitable to or prohibit me from working with children
-                </label>
-              </div>
-            </div>
-
-
-
-
-          </div>
-
-        </div>
 
 
 
@@ -186,12 +75,23 @@ const ProfileDetailsForm = ({ handleNext, handleBack, activeStep }) => {
         </div> */}
         {/* <button type="submit" class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button> */}
         <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-          <Button disabled={activeStep === 0} onClick={handlePrevious}>
-            Previous
+          <Button
+            color="inherit"
+            disabled={activeStep === 0}
+            onClick={handleBack}
+            sx={{ mr: 1 }}
+          >
+            Back
           </Button>
           <Box sx={{ flex: '1 1 auto' }} />
-          <Button type="submit" onClick={handleNext}>
-            Next step
+          {isStepOptional(activeStep) && (
+            <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+              Skip
+            </Button>
+          )}
+
+          <Button onClick={handleNext}>
+            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
           </Button>
         </Box>
       </form>
